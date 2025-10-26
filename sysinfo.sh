b3486af264a155fd28aa516e42f0d4567d7d6c1a
roman11x <romanmax1000@gmail.com>
2025-10-26 15:40:07 +0200

    Shebang, Description and Todo

---
+++ sysinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Arch System Information and Pakcge Management Tool
+#Author: Roman Nizkar, third year I.S student in Haifa University
+#Date: 26th of October, 2025
+#Description: Displays system information and manages packages for Arch/Arch based systems
+
+echo "Arch System Info Tool v.01"
+echo "========================="
+echo ""
+echo "Todo: System stats and package management"
+
+
